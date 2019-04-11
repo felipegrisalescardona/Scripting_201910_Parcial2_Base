9bc4332d6ee2cfb6eba2ae1941e9a029548a7fd3
felipegrisalescardona <felipe-grisales@hotmail.com>
2019-04-11 17:53:10 -0500

    Add files via upload

---
+++ Scripts/Game/Character.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHP;
+
+    [SerializeField]
+    private Bullet bullet;
+
+    [SerializeField]
+    private int numerobalas;
+
+    [SerializeField]
+    private float shootForce = 20F;
+
+    [SerializeField]
+    private Transform bulletSpawnPosition;
+
+    private Bullet[] Bullets;
+    private int ActualBullet;
+
+    private float hp;
+
+    public float HP
+    {
+        get { return hp; }
+        protected set { hp = value; }
+    }
+
+    public float ShootForce { get { return shootForce; } }
+
+    public void ModifyHP(float delta)
+    {
+        HP += delta;
+
+        if (HP <= 0F)
+        {
+            OnDeath();
+        }
+    }
+
+    protected virtual void OnDeath()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        CreatePool();
+        HP = maxHP;
+    }
+
+    protected void SpawnBullet()
+    {
+        if (bullet != null && bulletSpawnPosition != null)
+        {
+            Instantiate<Bullet>(bullet, bulletSpawnPosition.position, transform.rotation).Shoot(this);
+        }
+    }
+
+    protected void ShootActualBullet()
+    {
+        Bullet ActualBullet = GetNextBullet();
+        ActualBullet.transform.position = bulletSpawnPosition.position;
+        ActualBullet.transform.rotation = transform.rotation;
+        ActualBullet.Shoot(this);
+    }
+
+    public void CreatePool()
+    {
+        Bullets = new Bullet[numerobalas];
+        for (int i = 0; i < numerobalas; i++)
+        {
+            Bullet instanciabala = GameObject.Instantiate(bullet, new Vector3(i, 0, 100), Quaternion.identity);
+            Bullets[i] = instanciabala.GetComponent<Bullet>();
+        }
+    }
+
+    public Bullet GetNextBullet()
+    {
+        ActualBullet++;
+        if (ActualBullet >= numerobalas)
+        {
+            ActualBullet = 0;
+        }
+        return Bullets[ActualBullet];
+    }
+}
---
+++ Scripts/Game/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    float SpawnCd = 5;
+    [SerializeField]
+    Transform SpawnPoint;
+    [SerializeField]
+    AICharacter[] EnemigosAIntanciar;
+    int ActualEnemy = 0;
+    float timecounter;
+    
+
+
+    public AICharacter SendNextOne()
+    {
+        ActualEnemy++;
+        AICharacter NextOne;
+        if (ActualEnemy >= EnemigosAIntanciar.Length)
+        {
+            ActualEnemy = 0;
+        }
+        NextOne = EnemigosAIntanciar[ActualEnemy];
+
+        return NextOne;
+    }
+    public void SpawnSht()
+    {
+        AICharacter ActualEnemy = SendNextOne();
+        ActualEnemy.transform.position = SpawnPoint.position;
+        ActualEnemy.transform.rotation = transform.rotation;
+        ActualEnemy.ModifyHP(100);
+    }
+
+    void Update()
+    {
+        timecounter += Time.deltaTime;
+        if (timecounter >= SpawnCd)
+        {
+            Debug.Log(timecounter);
+            SpawnSht();
+            timecounter = 0;
+        }
+    }
+}
